192d946bba57319127db21bbda9bfb89f4014e2f
Binish Maharjan <binish.maharjan.lj@ambl.co.jp>
2025-07-27 08:15:01 +0900

    add calendarkit

--- App/TeamShift/Package.swift
+++ App/TeamShift/Package.swift
@@ -22,6 +22,7 @@ let package = Package(
         .library(name: "ClientUserStore", targets: ["ClientUserStore"]),
         .library(name: "ClientUserSession", targets: ["ClientUserSession"]),
         .library(name: "ClientUserDefaults", targets: ["ClientUserDefaults"]),
+        .library(name: "CalendarKit", targets: ["KitCalendar"]),
     ],
     dependencies: [
         .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", exact: "0.58.2"),
@@ -196,6 +197,17 @@ let package = Package(
         ),
         .target(
             name: "ClientUserDefaults",
+            dependencies: [
+                "SharedModels",
+                .product(name: "Dependencies", package: "swift-dependencies"),
+                .product(name: "DependenciesMacros", package: "swift-dependencies"),
+            ],
+            plugins: [
+                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins"),
+            ]
+        ),
+        .target(
+            name: "KitCalendar",
             dependencies: [
                 "SharedModels",
                 .product(name: "Dependencies", package: "swift-dependencies"),
---
+++ App/TeamShift/Sources/KitCalendar/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  TeamShift
+//
+//  Created by マハルジャン	ビニシュ on 2025/07/27.
+//
+
+import Foundation
